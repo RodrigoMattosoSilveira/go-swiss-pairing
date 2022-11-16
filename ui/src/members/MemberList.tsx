d4f232538154c7f17952a67ec09342105bb459f7
Rodrigo Silveira <r3850365@gmail.com>
2022-11-16 07:00:21 -0800

    00003-add-members-ui - Wrote logic to show members in a table

--- ui/src/members/MemberList.tsx
+++ ui/src/members/MemberList.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { Member } from './Member';
+import {MOCK_MEMBERS} from "./MOCK_MEMBERS";
 
 interface MemberListProps {
     members: Member[];
 }
 
-function ProjectList({ members }: MemberListProps) {
-    return <pre>{JSON.stringify(members, null, ' ')}</pre>;
+function MemberList({ members }: MemberListProps) {
+    // return <pre>{JSON.stringify(members, null, ' ')}</pre>;
+    return (
+        <>
+            <table className="hoverable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First</th>
+                        <th>Email</th>
+                        <th>Active</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {members.map((member) => (
+                        <tr>
+                            <td data-label="ID">{member.id}</td>
+                            <td data-label="First">{member.first}</td>
+                            <td data-label="Email">{member.email}</td>
+                            <td data-label="Active">{member.isActive ? "Yes" : "No"}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+
 }
 
-export default ProjectList;
+export default MemberList;
