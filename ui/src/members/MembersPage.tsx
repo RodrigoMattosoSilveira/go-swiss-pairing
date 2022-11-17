39d9730582188b06ee0e418e55f2f9eb4170adda
Rodrigo Silveira <r3850365@gmail.com>
2022-11-17 12:15:11 -0800

    00003-add-members-ui - Lab 05 Added data support;

--- ui/src/members/MembersPage.tsx
+++ ui/src/members/MembersPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {MOCK_MEMBERS} from "./MOCK_MEMBERS";
 
 function ProjectsPage() {
-    return <h1>Projects</h1>;
+    return (
+         <>
+               <h1>Projects</h1>
+              <pre>{JSON.stringify(MOCK_MEMBERS, null, ' ')}</pre>
+             </>
+        );
 }
 
 export default ProjectsPage;
