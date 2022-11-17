0dad4b28d766b202d3a3d6e36cab8d2f29240d5c
Rodrigo Silveira <r3850365@gmail.com>
2022-11-17 12:54:23 -0800

    00003-add-members-ui - Lab 06 Refactored to add members with all their attributes

--- ui/src/members/Member.ts
+++ ui/src/members/Member.ts
@@ -1,14 +1,22 @@
 export class Member {
     id: string | undefined;
     first: string = '';
+    last: string = '';
     email: string = '';
+    password: string = '';
+    cell: string = '';
+    rating: number = 1200;
     isActive: boolean = false
 
     constructor(initializer?: any) {
         if (!initializer) return;
         if (initializer.id) this.id = initializer.id;
         if (initializer.first) this.first = initializer.first;
+        if (initializer.last) this.last = initializer.last;
         if (initializer.email) this.email = initializer.email;
+        if (initializer.password) this.password = initializer.password;
+        if (initializer.cell) this.cell = initializer.cell;
+        if (initializer.rating) this.rating = initializer.rating;
         if (initializer.isActive) this.isActive = initializer.isActive;
     }
 }
