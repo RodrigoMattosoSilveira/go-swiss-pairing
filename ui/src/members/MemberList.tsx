639351a5123eb2fecb4bb112046eb287e5e9d957
Rodrigo Silveira <r3850365@gmail.com>
2022-11-18 15:26:50 -0800

    00003-add-members-ui - Lab 12: Hid and showed the Card component. I'll do a bit of work to try to do the same for the table component.

--- ui/src/members/MemberList.tsx
+++ ui/src/members/MemberList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Member } from './Member';
 import MemberCard from "./MemberCard";
 import MemberForm from "./MemberForm";
@@ -8,16 +8,23 @@ interface ProjectListProps {
 }
 
 function ProjectList({ members }: ProjectListProps) {
+    const [memberBeingEdited, setMemeberBeingEdited] = useState({});
     const handleEdit = (member: Member) => {
-        console.log(member);
+        // console.log(member);
+        setMemeberBeingEdited(member)
     }
     // return <pre>{JSON.stringify(members, null, ' ')}</pre>;
    return (
         <div className="row">
             {members.map((member) => (
             <div key={member.id} className="cols-sm">
-                <MemberCard member={member} onEdit={handleEdit}></MemberCard>
-                <MemberForm/>
+                {/*<MemberCard member={member} onEdit={handleEdit}></MemberCard>*/}
+                {/*<MemberForm/>*/}
+               {member === memberBeingEdited ? (
+                 <MemberForm />
+               ) : (
+                 <MemberCard member={member} onEdit={handleEdit} />
+               )}
             </div>
             ))}
         </div>
