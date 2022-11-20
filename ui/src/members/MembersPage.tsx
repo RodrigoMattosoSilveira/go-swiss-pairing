18d47399493a6242ea0e7f63ba15f22f4d70048a
Rodrigo Silveira <r3850365@gmail.com>
2022-11-20 13:53:31 -0800

    00003-add-members-ui - Lab 18 Implemented HTTP Get and pagination ... Loading does not work

--- ui/src/members/MembersPage.tsx
+++ ui/src/members/MembersPage.tsx
@@ -1,10 +1,37 @@
-import React, { Fragment, useState } from 'react';
-import { MOCK_MEMBERS } from "../../src/members/MOCK_MEMBERS";
+import React, { Fragment, useState, useEffect } from 'react';
 import MemberList from "../../src/members/MemberList"
 import { Member } from "./Member"
+import { memberAPI } from "./memberAPI";
 
 function MembersPage() {
-    const [members, setMembers] = useState<Member[]>(MOCK_MEMBERS);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [members, setMembers] = useState<Member[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    useEffect(() => {
+        async function loadProjects() {
+            setMembers([]);
+            setLoading(true);
+            try {
+                const data = await memberAPI.get(currentPage);
+                setError('');
+                setMembers(data);
+                if (currentPage === 1) {
+                    setMembers(data);
+                } else {
+                    setMembers((members) => [...members, ...data]);
+                }
+            }
+            catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, [currentPage]);
     const saveMember = (member: Member) => {
         // console.log('Saving project: ', member);
         let updatedMembers = members.map((m: Member) => {
@@ -12,11 +39,43 @@ function MembersPage() {
         });
         setMembers(updatedMembers);
     };
+    const handleMoreClick = () => {
+        setCurrentPage((currentPage) => currentPage + 1);
+    };
     return (
         <>
             <h1>Members</h1>
             {/* <pre>{JSON.stringify(MOCK_MEMBERS, null, ' ')}</pre> */}
+            {error && (
+                <div className="row">
+                    <div className="card large error">
+                        <section>
+                            <p>
+                                <span className="icon-alert inverse "></span>
+                                {error}
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            )}
             <MemberList members={members} onSave={saveMember}/>
+           {loading && (
+                <div className="center-page">
+                    <span className="spinner primary"></span>
+                    <p>Loading...</p>
+                </div>
+           )}
+            {!loading && !error && (
+            <div className="row">
+                <div className="col-sm-12">
+                <div className="button-group fluid">
+                    <button className="button default" onClick={handleMoreClick}>
+                        More...
+                    </button>
+                </div>
+                </div>
+            </div>
+            )}
         </>
     );
 }
