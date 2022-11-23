2f1afaeff3664d0832faa38880753d5b2712b691
Rodrigo Silveira <r3850365@gmail.com>
2022-11-22 16:03:09 -0800

    Testing Lab 2: Implemented Snapshot Tests; they did not create the snapshot as suggested in the LAB 02; I wrote a note to the author.

---
+++ ui/src/home/HomePage.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import HomePage from './HomePage';
+
+describe('<HomePage />', () => {
+    test('renders home heading', () => {
+        // render(<HomePage/>);
+        // expect(screen.getByRole('heading')).toHaveTextContent('Home');
+        test('should render without crashing', () => {
+            render(<HomePage />);
+        });
+    });
+    test('snapshot', () => {
+      const tree = renderer.create(<HomePage />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+});
