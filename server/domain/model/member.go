1576b5ebc1422f5c9e3da0827a754629972978cf
Rodrigo Silveira <r3850365@gmail.com>
2022-12-01 20:03:22 -0800

    00032-remaining-member-attributes Refactored the server/domain/model/member.go file to include all attributes

--- server/domain/model/member.go
+++ server/domain/model/member.go
@@ -1,16 +1,38 @@
 package model
 
 type Member struct {
-	id    string
-	first string
-	email string
+	id       string
+	first    string
+	last     string
+	email    string
+	password string
+	cell     string
+	rating   int32
+	isActive bool
+	imageUrl string
 }
 
-func Create(id string, first string, email string) *Member {
+func Create(
+	id string,
+	first string,
+	last string,
+	email string,
+	password string,
+	cell string,
+	rating int32,
+	isActive bool,
+	imageUrl string,
+) *Member {
 	return &Member{
-		id:    id,
-		first: first,
-		email: email,
+		id:       id,
+		first:    first,
+		last:     last,
+		email:    email,
+		password: password,
+		cell:     cell,
+		rating:   rating,
+		isActive: isActive,
+		imageUrl: imageUrl,
 	}
 }
 
@@ -22,8 +44,32 @@ func (cm Member) First() string {
 	return cm.first
 }
 
+func (cm Member) Last() string {
+	return cm.last
+}
+
 func (cm Member) Email() string {
 	return cm.email
+}
+
+func (cm Member) Password() string {
+	return cm.password
+}
+
+func (cm Member) Cell() string {
+	return cm.cell
+}
+
+func (cm Member) Rating() int32 {
+	return cm.rating
+}
+
+func (cm Member) IsActive() bool {
+	return cm.isActive
+}
+
+func (cm Member) ImageUrl() string {
+	return cm.imageUrl
 }
 
 func (cm Member) Empty() {}
